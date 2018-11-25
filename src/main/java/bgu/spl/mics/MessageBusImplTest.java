581775599ea1053c5f245f6405fab7ab8748dd31
orly <dadashev@post.bgu.ac.il>
2018-11-25 18:32:05 +0200

    --

---
+++ src/main/java/bgu/spl/mics/MessageBusImplTest.java
@@ -0,0 +1,40 @@
+package bgu.spl.mics;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MessageBusImplTest {
+
+    @Test
+    public void subscribeEvent() {
+    }
+
+    @Test
+    public void subscribeBroadcast() {
+    }
+
+    @Test
+    public void complete() {
+    }
+
+    @Test
+    public void sendBroadcast() {
+    }
+
+    @Test
+    public void sendEvent() {
+    }
+
+    @Test
+    public void register() {
+    }
+
+    @Test
+    public void unregister() {
+    }
+
+    @Test
+    public void awaitMessage() {
+    }
+}
