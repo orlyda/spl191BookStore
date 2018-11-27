91b8b67cc9521f25b67cc9ef7f5b33f476354c1d
orly <dadashev@post.bgu.ac.il>
2018-11-27 12:21:11 +0200

    --

--- src/tests/MessageBusImplTest.java
+++ src/tests/MessageBusImplTest.java
@@ -2,7 +2,6 @@ package bgu.spl.mics;
 
 import bgu.spl.mics.application.services.InventoryService;
 import bgu.spl.mics.application.services.SellingService;
-import bgu.spl.mics.example.messages.ExampleBroadcast;
 import bgu.spl.mics.example.messages.ExampleEvent;
 import org.junit.Assert;
 import org.junit.Test;
@@ -23,28 +22,12 @@ public class MessageBusImplTest {
     }
 
     @Test
-    public void subscribeBroadcast() {
-    }
-
-    @Test
-    public void complete() {
-    }
-
-    @Test
-    public void sendBroadcast() {
-    }
-
-    @Test
     public void sendEvent() {
         Event<String> e=new ExampleEvent("Orly");
         MessageBusImpl m=new MessageBusImpl();
         Future<String> f=m.sendEvent(e);
         if(f!=null)
             Assert.fail();
-    }
-
-    @Test
-    public void unregister() {
     }
 
     @Test
