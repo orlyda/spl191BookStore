c99b86dd84264209d68150b082a16636c00d8f71
orly <dadashev@post.bgu.ac.il>
2018-12-13 16:16:39 +0200

    --

--- src/main/java/bgu/spl/mics/application/services/APIService.java
+++ src/main/java/bgu/spl/mics/application/services/APIService.java
@@ -3,6 +3,8 @@ package bgu.spl.mics.application.services;
 import bgu.spl.mics.Callback;
 import bgu.spl.mics.Future;
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.Pair;
+import bgu.spl.mics.application.messages.DeliveryEvent;
 import bgu.spl.mics.application.messages.OrderBookEvent;
 import bgu.spl.mics.application.messages.TerminateBroadcast;
 import bgu.spl.mics.application.messages.TickBroadcast;
@@ -46,36 +48,16 @@ public class APIService extends MicroService{
 	protected void initialize() {
 		Callback<TickBroadcast> tickCallback= t -> {
 			if(futureOrders.size()>0){
-				ExecutorService e=Executors.newFixedThreadPool(futureOrders.size());
-				CompletionService<OrderReceipt> cService=new ExecutorCompletionService<>(e);
-				int j =0;
-				synchronized (futureOrders){//take order from the list in the correct tick
-					// and send event that handle the order
-					while((!futureOrders.isEmpty()) && futureOrders.get(0).getTick()==t.getTick()) {
-						FutureOrder fut= futureOrders.remove(0);
-						cService.submit(new Callable<OrderReceipt>() {
-							@Override
-							public synchronized OrderReceipt call(){
-								Future<OrderReceipt> f = sendEvent(new OrderBookEvent(customer, fut.getBookTitle(),
-										fut.getTick(),customer.getAvailableCreditAmount()));
-								while (!f.isDone()) {
-									try {
-										wait(30);
-									} catch (InterruptedException e1) {
-										e1.printStackTrace();
-									}
-								}
-								return f.get();
-							}
-						});
-						j++;
-						}}
-				e.shutdown();
+				Pair<Integer,CompletionService<OrderReceipt>> p=tickManager(t.getTick());
+				int j=p.getFirst();
+				CompletionService<OrderReceipt> cService=p.getSecond();
 				while (j>0){
 					try {
 						OrderReceipt orderReceipt=cService.take().get();
-						if(orderReceipt.getPrice()!=-1)
+						if(orderReceipt.getPrice()!=-1) {
 							customer.addReceipt(orderReceipt);
+							sendEvent(new DeliveryEvent(customer.getAddress(),customer.getDistance()));
+						}
 						System.out.println("order completed");
 						j--;
 					} catch (Exception e1) {
@@ -86,6 +68,35 @@ public class APIService extends MicroService{
 		this.subscribeBroadcast(TerminateBroadcast.class, c-> this.terminate());
 	}
 
+	public Pair<Integer,CompletionService<OrderReceipt>> tickManager(int tick){
+		ExecutorService e=Executors.newFixedThreadPool(futureOrders.size());
+		CompletionService<OrderReceipt> cService=new ExecutorCompletionService<>(e);
+		int j =0;
+		synchronized (futureOrders){//take order from the list in the correct tick
+			// and send event that handle the order
+			while((!futureOrders.isEmpty()) && futureOrders.get(0).getTick()==tick) {
+				FutureOrder fut= futureOrders.remove(0);
+
+				cService.submit(new Callable<OrderReceipt>() {
+					@Override
+					public synchronized OrderReceipt call(){
+						Future<OrderReceipt> f = sendEvent(new OrderBookEvent(customer, fut.getBookTitle(),
+								fut.getTick(),customer.getAvailableCreditAmount()));
+						while (!f.isDone()) {
+							try {
+								wait(30);
+							} catch (InterruptedException e1) {
+								e1.printStackTrace();
+							}
+						}
+						return f.get();
+					}
+				});
+				j++;
+			}}
+		e.shutdown();
+		return (new Pair<>(j,cService));
+	}
 
 	public Customer getCustomer() {
 		return customer;
