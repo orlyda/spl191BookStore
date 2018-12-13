13a64543efe6b1e581e49fb01c9103eea3f252b5
orly <dadashev@post.bgu.ac.il>
2018-12-14 00:29:20 +0200

    --

--- src/main/java/bgu/spl/mics/application/services/APIService.java
+++ src/main/java/bgu/spl/mics/application/services/APIService.java
@@ -3,6 +3,7 @@ package bgu.spl.mics.application.services;
 import bgu.spl.mics.Callback;
 import bgu.spl.mics.Future;
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.Pair;
 import bgu.spl.mics.application.messages.DeliveryEvent;
 import bgu.spl.mics.application.messages.OrderBookEvent;
 import bgu.spl.mics.application.messages.TerminateBroadcast;
@@ -24,7 +25,7 @@ import java.util.concurrent.*;
  * You MAY change constructor signatures and even add new public constructors.
  */
 public class APIService extends MicroService{
-	public static final long waitingTime = 50;
+	private static final long waitingTime = 50;
 	private final Customer customer;
 	private ArrayList<FutureOrder> futureOrders;
 	//private List<Future<OrderReceipt>> futureList;
@@ -47,45 +48,53 @@ public class APIService extends MicroService{
 	@Override
 	protected void initialize(){
 		Callback<TickBroadcast> tickCallback= t -> {
-			if(futureOrders.size()>0){
-				ExecutorService e=Executors.newFixedThreadPool(futureOrders.size());
-				CompletionService<OrderReceipt> cService=new ExecutorCompletionService<>(e);
-				int j =0;
-				synchronized (futureOrders){//take order from the list in the correct tick
-					// and send event that handle the order
-					while((!futureOrders.isEmpty()) && futureOrders.get(0).getTick()==t.getTick()) {
-						FutureOrder fut= futureOrders.remove(0);
-						cService.submit(new Callable<OrderReceipt>() {
-							@Override
-							public synchronized OrderReceipt call(){
-								Future<OrderReceipt> f = sendEvent(new OrderBookEvent(customer, fut.getBookTitle(),
-										fut.getTick(),customer.getAvailableCreditAmount()));
-								while (!f.isDone()) {
-									try {
-										wait(waitingTime);
-									} catch (InterruptedException e1) {
-										e1.printStackTrace();
-									}
-								}
-								return f.get();
-							}});
-						j++; }}
-				e.shutdown();
-				while (j>0){
-					try {
+			if(futureOrders.size()>0){//check there are more orders to send
+				Pair<Integer,CompletionService<OrderReceipt>> p=tickManager(t.getTick());
+				int j=p.getFirst();
+				CompletionService<OrderReceipt> cService=p.getSecond();
+				while (j>0){//there are more orders that is not completed
+					try {//wait for all the orders to be completed, when order is complete, send the delivery
 						OrderReceipt orderReceipt=cService.take().get();
-						if(orderReceipt.getPrice()!=-1){
+						if(orderReceipt.getPrice()!=-1){//the book was in stock and the customer had enough money
 							customer.addReceipt(orderReceipt);
 							sendEvent(new DeliveryEvent(customer.getAddress(),customer.getDistance()));
 						}
-						System.out.println("order completed");
+						//System.out.println("order completed");
 						j--;
 					} catch (Exception e1) { e1.printStackTrace(); } }}
 		};
 		this.subscribeBroadcast(TickBroadcast.class,tickCallback);
 		this.subscribeBroadcast(TerminateBroadcast.class, c-> this.terminate());
 	}
+	/**When the tick broadcast happen,the tickManager checks if there are orders that need to be ordered,
+	if so,send all the events that supposed to be ordered in this tick*/
+	public Pair<Integer,CompletionService<OrderReceipt>> tickManager(int tick){
 
+		ExecutorService e=Executors.newFixedThreadPool(futureOrders.size());
+		CompletionService<OrderReceipt> cService=new ExecutorCompletionService<>(e);
+		int j =0;
+		synchronized (futureOrders){//take order from the list in the correct tick
+			// and send event that handle the order
+			while((!futureOrders.isEmpty()) && futureOrders.get(0).getTick()==tick) {
+				FutureOrder fut= futureOrders.remove(0);
+				cService.submit(new Callable<OrderReceipt>() {
+					@Override
+					public synchronized OrderReceipt call(){
+						Future<OrderReceipt> f = sendEvent(new OrderBookEvent(customer, fut.getBookTitle(),
+								fut.getTick(),customer.getAvailableCreditAmount()));
+						while (!f.isDone()) {
+							try {//wait until the order process is completed
+								wait(waitingTime);
+							} catch (InterruptedException e1) {
+								e1.printStackTrace();
+							}
+						}
+						return f.get();
+					}});
+				j++; }}
+		e.shutdown();
+		return new Pair<>(j,cService);
+	}
 
 	public Customer getCustomer() {
 		return customer;
